8a8002be637cca63f985ab47e6d5e0249150c57f
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-27 22:04:07 -0500

    update

--- install.js
+++ install.js
@@ -26,6 +26,7 @@ module.exports = {
         uri: "torch.js",
         params: {
           venv: "env",                // Edit this to customize the venv folder path
+          venv_python: "3.11",
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
         }
@@ -39,6 +40,28 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "uv pip install -r requirements.txt",
+        ]
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      when: "{{platform === 'win32'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.0.post2%2Bcu124torch2.5.1cxx11abiFALSE-cp311-cp311-win_amd64.whl"
+        ]
+      }
+    },
+    {
+      when: "{{platform === 'linux'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
           "uv pip install flash-attn --no-build-isolation",
         ]
       }
